df6895d5ac784c0e8f939a2cf6cecf23441cdd74
wbond <will@wbond.net>
2015-02-22 13:19:07 -0500

    Version 0.8.0

--- pybars/__init__.py
+++ pybars/__init__.py
@@ -26,7 +26,7 @@
 # established at this point, and setup.py will use a version of next-$(revno).
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
-__version__ = (0, 7, 2, 'final', 0)
+__version__ = (0, 8, 0, 'final', 0)
 
 __all__ = [
     'Compiler',
